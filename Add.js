b23f0f59a18c429d43141219812691f11f34da43
unknown <kemichi01@gmail.com>
2020-11-30 22:33:13 +0700

    ShowArr

--- Add.js
+++ Add.js
@@ -3,10 +3,9 @@ function showProDuct() {
     let content = "";
     for (let i = 0; i < product.length; i++) {
         content += '<tr>\n' +
-            '            <td>'+(i+1)+'</td>\n' +
             '            <td>'+ product[i] +'</td>\n' +
-            '            <td><button onclick="editProduct('+i+')">Edit</button></td>\n' +
-            '            <td><button onclick="deleteProduct()">Delete</button></td>\n' +
+            '            <td><button onclick="editProduct('+ i +')">Edit</button></td>\n' +
+            '            <td><button onclick="deleteProduct('+ i +')">Delete</button></td>\n' +
             '        </tr>'
     }
     document.getElementById("list").innerHTML = content;
@@ -17,11 +16,13 @@ function addProduct() {
     showProDuct();
     document.getElementById("input").value = "";
 }
-function editProduct() {
+function editProduct(index) {
     let a = prompt("input product");
     product[index] = a;
     showProDuct();
 }
-function deleteProduct() {
-    product.splice(index,1);
+function deleteProduct(index) {
+    console.log(index);
+ product.splice(index, 1);
+    showProDuct();
 }
